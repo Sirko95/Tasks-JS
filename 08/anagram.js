9240fd9df331a758323ff5089d5f07d76727fa19
Oleg Sirota <olegsirota95@gmail.com>
2017-12-12 18:53:45 +0200

    create folder for task 08

---
+++ 08/anagram.js
@@ -0,0 +1,11 @@
+function Anagram(anagram){
+    this.anagram = anagram;
+}
+
+Anagram.prototype.matches = function matches() {
+    for(var i = 0; i < arguments.length; i++) {
+        
+    }
+}
+
+module.exports = Anagram;
