fe7642f5b35691d34cd4666f8766d30b44f31043
Oleg Sirota <olegsirota95@gmail.com>
2017-12-13 11:34:51 +0200

    almost done task 08 except unit test "does not detect false positives"

--- 08/anagram.js
+++ 08/anagram.js
@@ -4,16 +4,33 @@ function Anagram(anagram){
 
 Anagram.prototype.matches = function matches() {
     let mass = []
+    let requiet = false;
+    if( arguments.length > 1 || typeof arguments[0] == 'string') {
+        for(var i = 0; i < arguments.length; i++) {
+            mass.push(arguments[i]);
+            if(i > 0) {
+                delete arguments[i];
+            }
+        }
+        arguments[0] = mass;
+        mass = [];
+    }
     for(var i = 0; i < arguments[0].length; i++) {
         if(arguments[0][i].length == this.anagram.length) {
+           const lowerArg = arguments[0][i].toLowerCase();
+           const lowerAnagr = this.anagram.toLowerCase();
+           requiet = true;
            for(var j = 0; j < this.anagram.length; j++) {
-               if(arguments[0][i].indexOf(this.anagram[j]) == -1) {
-                   return mass
+               if(lowerArg.indexOf(lowerAnagr[j]) == -1) {
+                   requiet = false;
                }
            }
-           mass.push(arguments[0][i]);
-        } else return mass
+           if(requiet && lowerAnagr != lowerArg) {
+                mass.push(arguments[0][i]);           
+           }
+        }
     }
+    return mass
 }
 
 module.exports = Anagram;
